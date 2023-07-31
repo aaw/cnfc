b1a743f2df886ab5e69a0b38793e1de3bc33312c
Aaron Windsor <aaron.windsor@gmail.com>
2023-07-31 07:18:59 -0400

    add an LRU cache for clauses

--- cnfc/cache.py
+++ cnfc/cache.py
@@ -1,16 +1,24 @@
 # An LRU cache mapping Expr -> Var
 
+from collections import OrderedDict
+
 class Cache:
     def __init__(self):
-        pass
+        self.d = OrderedDict()
 
-    # Returns value associated with key, or None if no (key, value) pair exists
+    def __len__(self):
+        return len(self.d)
+
     def get(self, key):
-        pass
+        value = self.d.get(key)
+        if value is not None:
+            self.d.move_to_end(key, last=False)
+        return value
 
     def put(self, key, value):
-        pass
+        self.d[key] = value
+        self.d.move_to_end(key, last=False)
 
     # Returns the least recently used (key, value) pair.
     def pop(self):
-        pass
+        return self.d.popitem()
---
+++ tests/test_cache.py
@@ -0,0 +1,30 @@
+from cache import Cache
+import unittest
+
+class TestCache(unittest.TestCase):
+    def test_put_get(self):
+        c = Cache()
+        self.assertEqual(c.get('a'), None)
+        c.put('a',1)
+        self.assertEqual(c.get('a'), 1)
+        c.put('b',2)
+        self.assertEqual(c.get('b'), 2)
+        c.put('a',3)
+        self.assertEqual(c.get('a'), 3)
+
+    def test_pop(self):
+        c = Cache()
+        c.put('a',1)
+        c.put('b',2)
+        c.put('c',3)
+        self.assertEqual(c.pop(), ('a',1))
+        self.assertEqual(c.pop(), ('b',2))
+        self.assertEqual(c.pop(), ('c',3))
+
+        c.put('a',1)
+        c.put('b',2)
+        c.put('c',3)
+        c.get('a')
+        self.assertEqual(c.pop(), ('b',2))
+        self.assertEqual(c.pop(), ('c',3))
+        self.assertEqual(c.pop(), ('a',1))
