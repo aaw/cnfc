6c045ed597939acf0ca7f583c1bbbed7fc24b011
Aaron Windsor <aaron.windsor@gmail.com>
2023-08-03 09:18:42 -0400

    using io.StringIO instead of a tempfile in tests

--- tests/test_buffer.py
+++ tests/test_buffer.py
@@ -1,21 +1,23 @@
 from buffer import Buffer
+import io
 import tempfile
 import unittest
+
+def flush_buffer_to_str(b):
+    f = io.StringIO()
+    b.Flush(f)
+    f.seek(0)
+    return f.read()
 
 class TestBuffer(unittest.TestCase):
     def test_something(self):
         b = Buffer()
         b.Append((1,2,3))
         b.Append((-1,-2))
-        # TODO: use io.StringIO here instead
-        with tempfile.NamedTemporaryFile(mode='w+t') as f:
-            b.Flush(f)
-            f.seek(0)
-            contents = f.read()
 
         expected = (
             'p cnf 3 2\n' +
             '1 2 3 0\n' +
             '-1 -2 0\n'
         )
-        self.assertEqual(contents, expected)
+        self.assertEqual(flush_buffer_to_str(b), expected)
