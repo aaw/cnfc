615c76c9bbb72efb7e798ec3f95e0e800e205972
Aaron Windsor <aaron.windsor@gmail.com>
2023-08-05 17:09:42 -0400

    add another conjunction/disjunction test

--- tests/test_formula.py
+++ tests/test_formula.py
@@ -167,13 +167,25 @@ class TestFormula(unittest.TestCase):
         f.AddClause(y)
         self.assertUnsat(f)
 
-    def test_embedded_conjunction_disjunction(self):
+    def test_embedded_disjunction_in_conjunction(self):
         f = Formula()
         x,y,z,w = f.AddVars('x,y,z,w')
         f.AddClause((x | y) & (z | w))
         self.assertSat(f)
         f.AddClause(~x)
         f.AddClause(~z)
+        self.assertSat(f)
+        f.AddClause(~y)
+        self.assertUnsat(f)
+
+    def test_embedded_conjunction_in_disjunction(self):
+        f = Formula()
+        x,y,z,w = f.AddVars('x,y,z,w')
+        f.AddClause((x & y) | (z & w))
+        self.assertSat(f)
+        f.AddClause(x)
+        f.AddClause(~z)
+        f.AddClause(~w)
         self.assertSat(f)
         f.AddClause(~y)
         self.assertUnsat(f)
