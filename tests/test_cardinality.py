856f00c697f1f6ffe3fac9279cb05220e9c547e5
Aaron Windsor <aaron.windsor@gmail.com>
2023-08-06 15:13:29 -0400

    more cardinality tests (one still failing)

--- tests/test_cardinality.py
+++ tests/test_cardinality.py
@@ -12,7 +12,7 @@ def write_cnf_to_string(f):
     out.seek(0)
     return out.read()
 
-class TestCache(unittest.TestCase):
+class TestCardinality(unittest.TestCase):
     # TODO: test_formula.py uses these two helpers too, extract them.
     def assertSat(self, formula):
         self.assertTrue(Satisfiable(write_cnf_to_string(formula)))
@@ -20,7 +20,7 @@ class TestCache(unittest.TestCase):
     def assertUnsat(self, formula):
         self.assertFalse(Satisfiable(write_cnf_to_string(formula)))
 
-    def test_exact(self):
+    def test_exact_basic(self):
         f = Formula()
         x,y,z,w = f.AddVars('x,y,z,w')
         exactly_n_true(f, [x,y,z,w], 2)
@@ -48,3 +48,74 @@ class TestCache(unittest.TestCase):
         f.AddClause(y)
         f.AddClause(~w)
         self.assertUnsat(f)
+
+    def test_at_least_basic(self):
+        f = Formula()
+        x,y,z = f.AddVars('x,y,z')
+        at_least_n_true(f, [x,y,z], 2)
+        self.assertSat(f)
+        f.AddClause(x)
+        f.AddClause(~y)
+        f.AddClause(z)
+        self.assertSat(f)
+
+        f = Formula()
+        x,y,z = f.AddVars('x,y,z')
+        at_least_n_true(f, [x,y,z], 2)
+        f.AddClause(x)
+        f.AddClause(~y)
+        f.AddClause(~z)
+        self.assertUnsat(f)
+
+        f = Formula()
+        x,y,z = f.AddVars('x,y,z')
+        at_least_n_true(f, [x,y,z], 2)
+        f.AddClause(~x)
+        f.AddClause(~y)
+        f.AddClause(~z)
+        self.assertUnsat(f)
+
+    def test_at_most_basic(self):
+        f = Formula()
+        x,y,z,w,v = f.AddVars('x,y,z,w,v')
+        at_most_n_true(f, [x,y,z,w,v], 2)
+        self.assertSat(f)
+        f.AddClause(x)
+        f.AddClause(~y)
+        f.AddClause(z)
+        f.AddClause(~w)
+        f.AddClause(~v)
+        self.assertSat(f)
+
+        f = Formula()
+        x,y,z,w,v = f.AddVars('x,y,z,w,v')
+        at_most_n_true(f, [x,y,z,w,v], 2)
+        self.assertSat(f)
+        f.AddClause(x)
+        f.AddClause(~y)
+        f.AddClause(z)
+        f.AddClause(~w)
+        f.AddClause(v)
+        self.assertUnsat(f)
+
+        f = Formula()
+        x,y,z,w,v = f.AddVars('x,y,z,w,v')
+        at_most_n_true(f, [x,y,z,w,v], 2)
+        self.assertSat(f)
+        f.AddClause(x)
+        f.AddClause(y)
+        f.AddClause(z)
+        f.AddClause(~w)
+        f.AddClause(v)
+        self.assertUnsat(f)
+
+        f = Formula()
+        x,y,z,w,v = f.AddVars('x,y,z,w,v')
+        at_most_n_true(f, [x,y,z,w,v], 2)
+        self.assertSat(f)
+        f.AddClause(~x)
+        f.AddClause(~y)
+        f.AddClause(~z)
+        f.AddClause(~w)
+        f.AddClause(v)
+        self.assertSat(f)
