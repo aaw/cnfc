6de2e636de1110020165fda6611942cef6f03e27
Aaron Windsor <aaron.windsor@gmail.com>
2023-08-13 10:04:30 -0400

    split AddClause and Add[SubFormula-converted-to-clauses] into two seperate methods

--- README.md
+++ README.md
@@ -7,16 +7,16 @@ from cnfc import *
 f = Formula()
 x, y, z, w = f.AddVars('x,y,z,w')
 
-f.AddClause(~x,y)  # Equivalent to f.AddClause(Or(~x,y))
+f.AddClause(~x,y)  # Equivalent to f.Add(Or(~x,y))
 f.AddClause(~y,z)
 f.AddClause(~z,x)
 
-# Same as f.AddClause(Or(Implies(x,y),Not(And(z,w))))
-f.AddClause(Implies(x,y) | ~(z & w))
+# Same as f.Add(Or(Implies(x,y),Not(And(z,w))))
+f.Add(Implies(x,y) | ~(z & w))
 
-f.AddClause(NumTrue(x,y,z) >= 1)
+f.Add(NumTrue(x,y,z) >= 1)
 
-f.AddClause(Tuple(x,y) < Tuple(z,w))
+f.Add(Tuple(x,y) < Tuple(z,w))
 
 with fd as open('/tmp/output.cnf', 'w'):
     f.WriteCNF(fd)
--- cnfc/formula.py
+++ cnfc/formula.py
@@ -21,19 +21,14 @@ class Formula:
     def AddVars(self, names):
         return (self.AddVar(name.strip()) for name in names.split(','))
 
+    def AddClause(self, *disjuncts):
+        self.buffer.Append(tuple(self.__raw_lit(x) for x in disjuncts))
+
     # TODO: perform light optimizations like removing duplicate literals,
     # suppressing tautologies, and supressing duplicate clauses
-    def AddClause(self, *expr):
-        # TODO: ensure all variables exist in self.vars
-        def raw_lit(expr):
-            if isinstance(expr, Var): return expr.vid
-            elif isinstance(expr, Literal): return expr.sign*expr.var.vid
-            else: raise ValueError("Expected Var or Literal, got {}".format(expr))
-        if len(expr) > 1:
-            self.buffer.Append(tuple(raw_lit(x) for x in expr))
-        else:
-            for clause in expr[0].generate_cnf(self):
-                self.buffer.Append(tuple(raw_lit(x) for x in clause))
+    def Add(self, expr):
+        for clause in expr.generate_cnf(self):
+            self.buffer.Append(tuple(self.__raw_lit(x) for x in clause))
 
     def PushCheckpoint(self):
         self.buffer.PushCheckpoint()
@@ -43,3 +38,8 @@ class Formula:
 
     def WriteCNF(self, fd):
         self.buffer.Flush(fd)
+
+    def __raw_lit(self, expr):
+        if isinstance(expr, Var): return expr.vid
+        elif isinstance(expr, Literal): return expr.sign*expr.var.vid
+        else: raise ValueError("Expected Var or Literal, got {}".format(expr))
