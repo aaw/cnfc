ed44c6670222a6051976e9f46dde7c5cf3b20fa2
Aaron Windsor <aaron.windsor@gmail.com>
2023-08-14 11:55:03 -0400

    improve exhaustive tuple test to compare all tuples of length 2 and 3

--- tests/test_tuple.py
+++ tests/test_tuple.py
@@ -3,6 +3,7 @@ from cnfc import *
 from .dpll import Satisfiable
 
 import io
+import itertools
 import unittest
 
 # TODO: test_formula.py uses this, extract it.
@@ -20,37 +21,39 @@ class TestCardinality(unittest.TestCase):
     def assertUnsat(self, formula):
         self.assertFalse(Satisfiable(write_cnf_to_string(formula)))
 
+    # Test all dimension 2 and 3 tuples with < and <=.
     def test_less_than_small_exhaustive(self):
         f = Formula()
         x1,x2,y1,y2 = f.AddVars('x1,x2,y1,y2')
 
-        for x1v in (True,False):
-            for x2v in (True,False):
-                for y1v in (True,False):
-                    for y2v in (True,False):
-                        f.PushCheckpoint()  # setting (x1,x2,y1,y2)
+        for dimension in (2,3):
+            f = Formula()
+            x = list(f.AddVars(','.join('x{}'.format(i) for i in range(dimension))))
+            y = list(f.AddVars(','.join('y{}'.format(i) for i in range(dimension))))
 
-                        f.AddClause(x1) if x1v else f.AddClause(~x1)
-                        f.AddClause(x2) if x2v else f.AddClause(~x2)
-                        f.AddClause(y1) if y1v else f.AddClause(~y1)
-                        f.AddClause(y2) if y2v else f.AddClause(~y2)
+            for xv in itertools.product([True,False], repeat=dimension):
+                for yv in itertools.product([True,False], repeat=dimension):
+                    f.PushCheckpoint()  # setting x1,x2,... and y1,y2,...
+                    for i in range(dimension):
+                        f.AddClause(x[i]) if xv[i] else f.AddClause(~x[i])
+                        f.AddClause(y[i]) if yv[i] else f.AddClause(~y[i])
 
-                        f.PushCheckpoint()  # <= comparison
-                        for clause in tuple_less_than(f, (x1,x2), (y1,y2), strict=False):
-                            f.AddClause(*clause)
-                        if (x1v,x2v) <= (y1v,y2v):
-                            self.assertSat(f)
-                        else:
-                            self.assertUnsat(f)
-                        f.PopCheckpoint()  # <= comparison
+                    f.PushCheckpoint()  # <= comparison
+                    for clause in tuple_less_than(f, x, y, strict=False):
+                        f.AddClause(*clause)
+                    if xv <= yv:
+                        self.assertSat(f)
+                    else:
+                        self.assertUnsat(f)
+                    f.PopCheckpoint()  # <= comparison
 
-                        f.PushCheckpoint()  # < comparison
-                        for clause in tuple_less_than(f, (x1,x2), (y1,y2), strict=True):
-                            f.AddClause(*clause)
-                        if (x1v,x2v) < (y1v,y2v):
-                            self.assertSat(f)
-                        else:
-                            self.assertUnsat(f)
-                        f.PopCheckpoint()  # < comparison
+                    f.PushCheckpoint()  # < comparison
+                    for clause in tuple_less_than(f, x, y, strict=True):
+                        f.AddClause(*clause)
+                    if xv < yv:
+                        self.assertSat(f)
+                    else:
+                        self.assertUnsat(f)
+                    f.PopCheckpoint()  # < comparison
 
-                        f.PopCheckpoint()  # setting (x1,x2,y1,y2)
+                    f.PopCheckpoint()  # setting (x1,x2,y1,y2)
